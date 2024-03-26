514d353916750f1516fea40f505a2d4d1a542fde
rkdcodus <rcd7275@naver.com>
2024-03-27 04:36:27 +0900

    feat: 반응형 네비게이션 바 구현

--- week10/main.js
+++ week10/main.js
@@ -1,23 +1,31 @@
-const nav = document.getElementById("nav");
+const nav = document.querySelector(".nav");
 const section1 = document.querySelector(".section1").offsetTop;
 const section2 = document.querySelector(".section2").offsetTop;
 const section3 = document.querySelector(".section3").offsetTop;
 const section4 = document.querySelector(".section4").offsetTop;
 const button = document.getElementById("mainButton");
+const menu = document.getElementById("menu");
+const menuClose = document.getElementById("menu-close");
 
-const clickHandle = (e) => {
-  if (e.target.id === "home") {
-    window.scrollTo({ top: section1, behavior: "smooth" });
-  } else if (e.target.id === "about") {
-    window.scrollTo({ top: section2, behavior: "smooth" });
-  } else if (e.target.id === "product") {
-    window.scrollTo({ top: section3, behavior: "smooth" });
-  } else if (e.target.id === "contactUs") {
-    window.scrollTo({ top: section4, behavior: "smooth" });
-  }
+const scrollEvent = (top) => {
+  window.scrollTo({ top: top, behavior: "smooth" });
+  nav.classList.remove("open");
 };
 
-nav.addEventListener("click", clickHandle);
+const clickHandle = (e) => {
+  if (e.target.id === "home") scrollEvent(section1);
+  else if (e.target.id === "about") scrollEvent(section2);
+  else if (e.target.id === "product") scrollEvent(section3);
+  else if (e.target.id === "contactUs") scrollEvent(section4);
+};
+
 button.addEventListener("click", () => {
-  window.scrollTo({ top: section4, behavior: "smooth" });
+  scrollEvent(section4);
 });
+menu.addEventListener("click", () => {
+  nav.classList.add("open");
+});
+menuClose.addEventListener("click", () => {
+  nav.classList.remove("open");
+});
+nav.addEventListener("click", clickHandle);
