26df018cf4812d6a6ac7050e34fff50881a02ab8
rkdcodus <rcd7275@naver.com>
2024-04-30 16:38:36 +0900

    ✨feat: 이미지 불러오기 기능 추가 및 UI 구현

--- week15/app.js
+++ week15/app.js
@@ -1,23 +1,53 @@
+const datas = [
+  {
+    id: 0,
+    url: "https://wallpapercave.com/wp/aWaXW72.jpg",
+  },
+  {
+    id: 1,
+    url: "https://wallpapercave.com/wp/wp3181476.jpg",
+  },
+  {
+    id: 2,
+    url: "https://wallpapercave.com/wp/wp3181484.jpg",
+  },
+  {
+    id: 3,
+    url: "https://wallpapercave.com/wp/wp2123889.jpg",
+  },
+  {
+    id: 4,
+    url: "https://wallpapercave.com/wp/wp2209011.jpg",
+  },
+];
 const image = document.querySelector(".image_wrap");
+
 const next = document.getElementById("next");
 const prev = document.getElementById("prev");
 let center = 0;
 
-const next_slide = () => {
+const nextSlide = () => {
   center += 1;
-  if (center > 5 - 1) {
-    center = 5 - 1;
+  if (center > datas.length - 1) {
+    center = datas.length - 1;
   }
-  image.style.transform = `translate(-${center * 80}vw)`;
+  image.style.transform = `translate(-${center * 100}vw)`;
 };
 
-const prev_slide = () => {
+const prevSlide = () => {
   center -= 1;
   if (center < 0) {
     center = 0;
   }
-  image.style.transform = `translate(-${center * 80}vw)`;
+  image.style.transform = `translate(-${center * 100}vw)`;
 };
 
-next.addEventListener("click", next_slide);
-prev.addEventListener("click", prev_slide);
+const createImage = () => {
+  datas.map((data) => {
+    image.insertAdjacentHTML("beforeend", `<img class="image" src='${data.url}'></img>`);
+  });
+};
+
+next.addEventListener("click", nextSlide);
+prev.addEventListener("click", prevSlide);
+window.addEventListener("DOMContentLoaded", createImage);
