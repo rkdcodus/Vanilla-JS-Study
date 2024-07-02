b907dd68858f6f0beb3e6fe53d5297688ef666e1
rkdcodus <rcd7275@naver.com>
2024-07-03 02:24:15 +0900

    ✨feat: 아이템 delete 기능

--- week14/rkdcodus/app.js
+++ week14/rkdcodus/app.js
@@ -1,21 +1,52 @@
 const input = document.getElementById("input");
 const button = document.getElementById("button");
 const list = document.getElementById("list");
-const itemList = [];
+const itemCount = 0;
+let itemList = [];
 
-const createItem = (text, id) => {
+// li 태그 생성 함수
+const createHTML = (text, id) => {
+  itemCount += 1;
   const newItem = `
         <li id=${id}>
           <span id="itemText">${text}</span>
           <button id="edit">수정</button>
           <button id="remove">삭제</button>
         </li>`;
+
   return newItem;
 };
 
-button.addEventListener("click", () => {
-  itemList.push(input.value);
-  const newItem = createItem(input.value, itemList.indexOf(input.value));
+// 아이템 추가 함수
+const createItem = () => {
+  const text = input.value;
+  const newItem = createHTML(text, itemList.indexOf(text));
 
+  itemList.push(text);
   list.insertAdjacentHTML("beforeend", newItem);
-});
+};
+
+// 아이템 삭제 함수
+const deleteItem = (target) => {
+  list.removeChild(target);
+  delete itemList[target.id];
+  itemCount -= 1;
+
+  if (itemCount === 0) itemList = [];
+};
+
+// 아이템 수정 함수
+const editItem = () => {};
+
+// 아이템 삭제 or 수정 결정
+const clickItem = (e) => {
+  const option = e.target.id;
+  const selectedItem = e.target.parentNode;
+
+  if (option === "remove") deleteItem(selectedItem);
+  else if (option === "edit") {
+  }
+};
+
+button.addEventListener("click", createItem);
+list.addEventListener("click", clickItem);
