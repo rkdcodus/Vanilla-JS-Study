edeef28ea435d2a29ada3e0f7f9f3c994299fd93
rkdcodus <rcd7275@naver.com>
2024-07-03 01:46:31 +0900

    ✨feat: 생성되는 item 마다 버튼 추가

--- week14/rkdcodus/app.js
+++ week14/rkdcodus/app.js
@@ -3,10 +3,19 @@ const button = document.getElementById("button");
 const list = document.getElementById("list");
 const itemList = [];
 
+const createItem = (text, id) => {
+  const newItem = `
+        <li id=${id}>
+          <span id="itemText">${text}</span>
+          <button id="edit">수정</button>
+          <button id="remove">삭제</button>
+        </li>`;
+  return newItem;
+};
+
 button.addEventListener("click", () => {
   itemList.push(input.value);
-  const newLi = document.createElement("li");
-  newLi.textContent = input.value;
+  const newItem = createItem(input.value, itemList.indexOf(input.value));
 
-  list.insertAdjacentElement("beforeend", newLi);
+  list.insertAdjacentHTML("beforeend", newItem);
 });
