dba6619c25968dc06ea3f2902f10f40b1c446369
rkdcodus <rcd7275@naver.com>
2024-07-03 01:29:25 +0900

    ✨feat: 아이템 read 기능

--- week14/rkdcodus/app.js
+++ week14/rkdcodus/app.js
@@ -1,7 +1,12 @@
 const input = document.getElementById("input");
 const button = document.getElementById("button");
+const list = document.getElementById("list");
 const itemList = [];
 
 button.addEventListener("click", () => {
   itemList.push(input.value);
+  const newLi = document.createElement("li");
+  newLi.textContent = input.value;
+
+  list.insertAdjacentElement("beforeend", newLi);
 });
